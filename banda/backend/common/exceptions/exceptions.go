7c81fec6b5072333f21a5a74019a7b58a7c9a50c
Fikri R. Nurhidayat <fikrirnurhidayat@gmail.com>
2025-08-21 22:08:44 +0700

    chore: remove banda

--- banda/backend/common/exceptions/exceptions.go
+++
@@ -1,7 +0,0 @@
-package exceptions
-
-import "github.com/fikrirnurhidayat/banda/backend/common/exception"
-
-var (
-	SystemFailure = exception.Code(500).Reason("SYSTEM_FAILURE").Message("Unexpected behaviour occured on the system.").Error()
-)
--- banda/backend/common/spec/marshaler/sqlmarshaler/exception/exception.go
+++
@@ -1,9 +0,0 @@
-package exception
-
-import "github.com/fikrirnurhidayat/banda/backend/common/exception"
-
-var (
-	OperationNotSupported = exception.Reason("OPERATION_NOT_SUPPORTED")
-	FieldNotAllowed       = exception.Reason("FIELD_NOT_ALLOWED")
-	ValueNotAllowed       = exception.Reason("VALUE_NOT_ALLOWED")
-)
