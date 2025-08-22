ac08bcbdef6de8c365b8a06609e5966ef3dc9ac1
Fikri R. Nurhidayat <fikrirnurhidayat@gmail.com>
2025-08-22 15:14:34 +0700

    feat: dead simple transaction manager

--- banda/backend/src/main.rs
+++ banda/backend/src/main.rs
@@ -3,9 +3,11 @@ use std::net::SocketAddr;
 use axum::{
     Json, Router,
     extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
     routing::get,
 };
-use chrono::Utc;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, SqlitePool};
 use uuid::Uuid;
@@ -17,6 +19,21 @@ async fn main() {
     setup_database(&pool).await;
 
     let app = Router::new()
+        .route(
+            "/v1/categories",
+            get(search_categories).post(create_category),
+        )
+        .route(
+            "/v1/categories/:id",
+            get(get_category)
+                .put(update_category)
+                .delete(delete_category),
+        )
+        .route("/v1/sources", get(search_sources).post(create_source))
+        .route(
+            "/v1/sources/:id",
+            get(get_source).put(update_source).delete(delete_source),
+        )
         .route(
             "/v1/transactions",
             get(search_transactions).post(create_transaction),
@@ -39,6 +56,34 @@ async fn main() {
 async fn setup_database(pool: &SqlitePool) {
     sqlx::query(
         r#"
+    CREATE TABLE IF NOT EXISTS categories (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE,
+        created_at TEXT NOT NULL,
+        updated_at TEXT NOT NULL
+    )
+    "#,
+    )
+    .execute(pool)
+    .await
+    .unwrap();
+
+    sqlx::query(
+        r#"
+    CREATE TABLE IF NOT EXISTS sources (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE,
+        created_at TEXT NOT NULL,
+        updated_at TEXT NOT NULL
+    )
+    "#,
+    )
+    .execute(pool)
+    .await
+    .unwrap();
+
+    sqlx::query(
+        r#"
     CREATE TABLE IF NOT EXISTS transactions (
         id TEXT PRIMARY KEY,
         code TEXT NOT NULL,
@@ -46,9 +91,9 @@ async fn setup_database(pool: &SqlitePool) {
         amount INTEGER NOT NULL,
         frequency TEXT NOT NULL,
         kind TEXT NOT NULL,
-        category_id TEXT NOT NULL,
+        category_id TEXT NOT NULL REFERENCES categories (id),
         category_name TEXT NOT NULL,
-        source_id TEXT NOT NULL,
+        source_id TEXT NOT NULL REFERENCES sources (id),
         source_name TEXT NOT NULL,
         timestamp TEXT NOT NULL,
         created_at TEXT NOT NULL,
@@ -60,6 +105,248 @@ async fn setup_database(pool: &SqlitePool) {
     .await
     .unwrap();
 }
+
+// Common
+struct AppError(anyhow::Error);
+
+impl<E: Into<anyhow::Error>> From<E> for AppError {
+    fn from(err: E) -> Self {
+        AppError(err.into())
+    }
+}
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        eprintln!("Internal error: {:?}", self.0);
+        (StatusCode::NOT_IMPLEMENTED, ()).into_response()
+    }
+}
+
+// Category
+#[derive(Serialize, Deserialize, FromRow)]
+struct Category {
+    id: String,
+    name: String,
+    created_at: String,
+    updated_at: String,
+}
+
+#[derive(Deserialize)]
+struct CategoryRequest {
+    name: String,
+}
+
+async fn search_categories(
+    State(pool): State<SqlitePool>,
+) -> Result<Json<Vec<Category>>, AppError> {
+    let categories = sqlx::query_as::<_, Category>("SELECT * FROM categories")
+        .fetch_all(&pool)
+        .await?;
+    Ok(Json(categories))
+}
+
+async fn create_category(
+    State(pool): State<SqlitePool>,
+    Json(payload): Json<CategoryRequest>,
+) -> Result<Json<Category>, AppError> {
+    let id = Uuid::new_v4().to_string();
+    let now = Utc::now().to_rfc3339();
+
+    sqlx::query(
+        r#"INSERT INTO categories (
+            id,
+            name,
+            created_at,
+            updated_at
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?
+        )"#,
+    )
+    .bind(&id)
+    .bind(&payload.name)
+    .bind(&now)
+    .bind(&now)
+    .execute(&pool)
+    .await?;
+
+    let category = sqlx::query_as::<_, Category>("SELECT * FROM categories WHERE id = ?")
+        .bind(&id)
+        .fetch_one(&pool)
+        .await?;
+
+    Ok(Json(category))
+}
+
+async fn get_category(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+) -> Result<Json<Option<Category>>, AppError> {
+    let category = sqlx::query_as::<_, Category>("SELECT * FROM categories WHERE id = ?")
+        .bind(&id)
+        .fetch_optional(&pool)
+        .await?;
+
+    Ok(Json(category))
+}
+
+async fn update_category(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+    Json(payload): Json<CategoryRequest>,
+) -> Result<Json<Option<Category>>, AppError> {
+    let now = Utc::now().to_rfc3339();
+
+    let res = sqlx::query(
+        r#"
+    UPDATE categories
+    SET name = ?,
+        updated_at = ?
+    WHERE id = ?"#,
+    )
+    .bind(&payload.name)
+    .bind(&now)
+    .bind(&id)
+    .execute(&pool)
+    .await?;
+
+    if res.rows_affected() == 0 {
+        Ok(Json(None))
+    } else {
+        let categories = sqlx::query_as::<_, Category>("SELECT * FROM categories WHERE id = ?")
+            .bind(id)
+            .fetch_one(&pool)
+            .await?;
+        Ok(Json(Some(categories)))
+    }
+}
+
+async fn delete_category(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+) -> Result<(), AppError> {
+    sqlx::query("DELETE FROM categories WHERE id = ?")
+        .bind(id)
+        .execute(&pool)
+        .await?;
+    Ok(())
+}
+
+// Source
+#[derive(Serialize, Deserialize, FromRow)]
+struct Source {
+    id: String,
+    name: String,
+    created_at: String,
+    updated_at: String,
+}
+
+#[derive(Deserialize)]
+struct SourceRequest {
+    name: String,
+}
+
+async fn search_sources(State(pool): State<SqlitePool>) -> Result<Json<Vec<Source>>, AppError> {
+    let sources = sqlx::query_as::<_, Source>("SELECT * FROM sources")
+        .fetch_all(&pool)
+        .await?;
+    Ok(Json(sources))
+}
+
+async fn create_source(
+    State(pool): State<SqlitePool>,
+    Json(payload): Json<SourceRequest>,
+) -> Result<Json<Source>, AppError> {
+    let id = Uuid::new_v4().to_string();
+    let now = Utc::now().to_rfc3339();
+
+    sqlx::query(
+        r#"INSERT INTO sources (
+            id,
+            name,
+            created_at,
+            updated_at
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?
+        )"#,
+    )
+    .bind(&id)
+    .bind(&payload.name)
+    .bind(&now)
+    .bind(&now)
+    .execute(&pool)
+    .await?;
+
+    let source = sqlx::query_as::<_, Source>("SELECT * FROM sources WHERE id = ?")
+        .bind(&id)
+        .fetch_one(&pool)
+        .await?;
+
+    Ok(Json(source))
+}
+
+async fn get_source(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+) -> Result<Json<Option<Source>>, AppError> {
+    let source = sqlx::query_as::<_, Source>("SELECT * FROM sources WHERE id = ?")
+        .bind(&id)
+        .fetch_optional(&pool)
+        .await?;
+
+    Ok(Json(source))
+}
+
+async fn update_source(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+    Json(payload): Json<SourceRequest>,
+) -> Result<Json<Option<Source>>, AppError> {
+    let now = Utc::now().to_rfc3339();
+
+    let res = sqlx::query(
+        r#"
+    UPDATE sources
+    SET name = ?,
+        updated_at = ?
+    WHERE id = ?"#,
+    )
+    .bind(&payload.name)
+    .bind(&now)
+    .bind(&id)
+    .execute(&pool)
+    .await?;
+
+    if res.rows_affected() == 0 {
+        Ok(Json(None))
+    } else {
+        let sources = sqlx::query_as::<_, Source>("SELECT * FROM sources WHERE id = ?")
+            .bind(id)
+            .fetch_one(&pool)
+            .await
+            .unwrap();
+        Ok(Json(Some(sources)))
+    }
+}
+
+async fn delete_source(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+) -> Result<(), AppError> {
+    sqlx::query("DELETE FROM sources WHERE id = ?")
+        .bind(id)
+        .execute(&pool)
+        .await?;
+    Ok(())
+}
+
+// Transaction
 
 #[derive(Serialize, Deserialize, FromRow)]
 struct Transaction {
@@ -86,26 +373,37 @@ struct TransactionRequest {
     frequency: String,
     kind: String,
     category_id: String,
-    category_name: String,
     source_id: String,
-    source_name: String,
     timestamp: String,
 }
 
-async fn search_transactions(State(pool): State<SqlitePool>) -> Json<Vec<Transaction>> {
+async fn search_transactions(
+    State(pool): State<SqlitePool>,
+) -> Result<Json<Vec<Transaction>>, AppError> {
     let transactions = sqlx::query_as::<_, Transaction>("SELECT * FROM transactions")
         .fetch_all(&pool)
-        .await
-        .unwrap();
-    Json(transactions)
+        .await?;
+    Ok(Json(transactions))
 }
 
 async fn create_transaction(
     State(pool): State<SqlitePool>,
     Json(payload): Json<TransactionRequest>,
-) -> Json<Transaction> {
+) -> Result<Json<Transaction>, AppError> {
     let id = Uuid::new_v4().to_string();
     let now = Utc::now().to_rfc3339();
+    let timestamp = DateTime::parse_from_rfc3339(&payload.timestamp)?.to_rfc3339();
+
+    let source = sqlx::query_as::<_, Source>("SELECT * FROM sources WHERE id = ?")
+        .bind(&payload.source_id)
+        .fetch_one(&pool)
+        .await?;
+
+    let category = sqlx::query_as::<_, Category>("SELECT * FROM categories WHERE id = ?")
+        .bind(&payload.category_id)
+        .fetch_one(&pool)
+        .await?;
+
     sqlx::query(
         r#"INSERT INTO transactions (
             id,
@@ -143,47 +441,51 @@ async fn create_transaction(
     .bind(&payload.amount)
     .bind(&payload.frequency)
     .bind(&payload.kind)
-    .bind(&payload.category_id)
-    .bind(&payload.category_name)
-    .bind(&payload.source_id)
-    .bind(&payload.source_name)
-    .bind(&payload.timestamp)
+    .bind(&category.id)
+    .bind(&category.name)
+    .bind(&source.id)
+    .bind(&source.name)
+    .bind(&timestamp)
     .bind(&now)
     .bind(&now)
     .bind(&now)
     .execute(&pool)
-    .await
-    .unwrap()
-    .last_insert_rowid();
+    .await?;
 
     let transaction = sqlx::query_as::<_, Transaction>("SELECT * FROM transactions WHERE id = ?")
         .bind(&id)
         .fetch_one(&pool)
-        .await
-        .unwrap();
+        .await?;
 
-    Json(transaction)
+    Ok(Json(transaction))
 }
 
 async fn get_transaction(
     State(pool): State<SqlitePool>,
     Path(id): Path<String>,
-) -> Json<Option<Transaction>> {
+) -> Result<Json<Option<Transaction>>, AppError> {
     let transaction = sqlx::query_as::<_, Transaction>("SELECT * FROM transactions WHERE id = ?")
         .bind(&id)
         .fetch_optional(&pool)
-        .await
-        .unwrap();
+        .await?;
 
-    Json(transaction)
+    Ok(Json(transaction))
 }
 
 async fn update_transaction(
     State(pool): State<SqlitePool>,
     Path(id): Path<String>,
     Json(payload): Json<TransactionRequest>,
-) -> Json<Option<Transaction>> {
+) -> Result<Json<Option<Transaction>>, AppError> {
     let now = Utc::now().to_rfc3339();
+    let source = sqlx::query_as::<_, Source>("SELECT * FROM sources WHERE id = ?")
+        .bind(&payload.source_id)
+        .fetch_one(&pool)
+        .await?;
+    let category = sqlx::query_as::<_, Category>("SELECT * FROM categories WHERE id = ?")
+        .bind(&payload.category_id)
+        .fetch_one(&pool)
+        .await?;
 
     let res = sqlx::query(
         r#"
@@ -206,19 +508,18 @@ async fn update_transaction(
     .bind(&payload.amount)
     .bind(&payload.frequency)
     .bind(&payload.kind)
-    .bind(&payload.category_id)
-    .bind(&payload.category_name)
-    .bind(&payload.source_id)
-    .bind(&payload.source_name)
+    .bind(&category.id)
+    .bind(&category.name)
+    .bind(&source.id)
+    .bind(&source.name)
     .bind(&payload.timestamp)
     .bind(&now)
     .bind(&id)
     .execute(&pool)
-    .await
-    .unwrap();
+    .await?;
 
     if res.rows_affected() == 0 {
-        Json(None)
+        Ok(Json(None))
     } else {
         let transactions =
             sqlx::query_as::<_, Transaction>("SELECT * FROM transactions WHERE id = ?")
@@ -226,15 +527,17 @@ async fn update_transaction(
                 .fetch_one(&pool)
                 .await
                 .unwrap();
-        Json(Some(transactions))
+        Ok(Json(Some(transactions)))
     }
 }
 
-async fn delete_transaction(State(pool): State<SqlitePool>, Path(id): Path<String>) -> Json<bool> {
-    let res = sqlx::query("DELETE FROM transactions WHERE id = ?")
+async fn delete_transaction(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<String>,
+) -> Result<(), AppError> {
+    sqlx::query("DELETE FROM transactions WHERE id = ?")
         .bind(id)
         .execute(&pool)
-        .await
-        .unwrap();
-    Json(res.rows_affected() > 0)
+        .await?;
+    Ok(())
 }
